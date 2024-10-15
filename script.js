3800e289c40ebff6574524994bc003d5edab0ba6
yuldan14 <72258620+yuldan14@users.noreply.github.com>
2024-10-15 08:48:36 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -8,5 +8,28 @@ togglePassword.addEventListener("click", function(){
     this.classList.toggle("bi-eye");
 });
 
-// button cart
-  
+// Menu Burger
+
+const burger = document.getElementById('burger');
+const navLinks = document.getElementById('nav-links');
+
+// Toggle menu function
+const toggleMenu = () => {
+    navLinks.classList.toggle('active');
+    burger.classList.toggle('toggle');
+};
+
+// Burger menu click event
+burger.addEventListener('click', () => {
+    toggleMenu();
+}); 
+// Close menu when clicking outside
+document.addEventListener('click', (event) => {
+    const target = event.target;
+    const isNavMenu = target.closest('.nav-menu');
+
+    if (!isNavMenu) {
+        navLinks.classList.remove('active');
+        burger.classList.remove('toggle');
+    }
+});
